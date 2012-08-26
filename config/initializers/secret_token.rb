77268395d6ba9c1e8f707b0e60f3e186480981db
pradeepreddyk <pradeep.reddy.iitm@gmail.com>
2012-08-26 11:41:41 +0530

    init with inventory - spork/ guard/ pgsql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BorrowBar::Application.config.secret_token = '3d89762fa74fde4ccf7c31b8e268f6bb62bb3c70d6775a58a197202d69a4794ccd0983a232f8dc24c387bce71f2b7f2e158b73fef151787ed65d2ade688bc3af'
